714b8b67462e444259985fc796730fa0846fefb4
Vaughn Kottler <vaughn@libre-embedded.com>
2024-12-28 21:51:19 -0600

    Remove copied source.

--- addon/bars.lua
+++
@@ -1,9 +0,0 @@
-function ActionBarManagementInit(ui)
-	createButton(ui, "Audit Action Bars", 0, 3, function()
-		print("Audit Action Bars")
-	end)
-
-	createButton(ui, "Set Action Bars", 1, 3, function()
-		print("Set Action Bars")
-	end)
-end
