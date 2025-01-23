db6da47ff9ac52b88cf672e58d1e21108403ff13
Vaughn Kottler <vaughn@libre-embedded.com>
2025-01-22 22:06:16 -0600

    More addon dev

--- deploy_steamdeck.sh
+++ deploy_steamdeck.sh
@@ -6,7 +6,8 @@ CLIENT=retail
 ADDONS=_${CLIENT}_/Interface/AddOns
 
 # DEST=steamdeck
-DEST="192.168.1.85"
+# DEST="192.168.1.85"
+DEST="192.168.1.89"
 
 set -x
 time rsync --del -r ~/wow/$ADDONS/ $DEST:~/wow/$ADDONS
